ba2898a5607cc6193308ed9eeebc2bcf3755ec0d
Joseph Udonsak <joseph.udonsak@live.co.uk>
2020-11-13 14:03:04 +0100

    Controller for investmnent strategy

---
+++ app/Http/Controllers/StrategyController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\StrategyResource;
+use App\Models\Strategy;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class StrategyController extends Controller {
+
+    public function index() {
+
+        return StrategyResource::collection(Strategy::all());
+    }
+
+    public function store(Request $request) {
+
+        $strategy = Strategy::create($request->all());
+
+        return response()->json(
+            new StrategyResource($strategy),
+            Response::HTTP_CREATED
+        );
+    }
+
+    public function show(Strategy $strategy) {
+
+        return new StrategyResource($strategy);
+    }
+
+    public function update(Request $request, Strategy $strategy) {
+
+        $strategy->update(
+            $request->only(
+                [
+                    'type',
+                    'tenure',
+                    'yield',
+                    'relief'
+                ]
+            )
+        );
+
+        return new StrategyResource($strategy);
+    }
+
+    public function destroy(Strategy $strategy) {
+
+        $strategy->delete();
+
+        return response()->json(
+            null,
+            Response::HTTP_NO_CONTENT
+        );
+    }
+}
