39e1937ff2b8b0786a2958b4f13540ae5e2564fc
Joseph Udonsak <joseph.udonsak@live.co.uk>
2020-11-13 11:54:34 +0100

    Default timestamp from Strategies

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use App\Models\User;
-use App\Models\Wallet;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class UserFactory extends Factory {
--- database/migrations/2020_11_13_085538_create_strategies_table.php
+++ database/migrations/2020_11_13_085538_create_strategies_table.php
@@ -2,26 +2,31 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStrategiesTable extends Migration
-{
+class CreateStrategiesTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('strategies', function (Blueprint $table) {
-            $table->id();
-            $table->string('type');
-            $table->integer('tenure');
-            $table->decimal('yield');
-            $table->decimal('relief');
+    public function up() {
 
-            $table->timestamps();
-        });
+        Schema::create(
+            'strategies',
+            function (Blueprint $table) {
+
+                $table->id();
+                $table->string('type');
+                $table->integer('tenure');
+                $table->decimal('yield');
+                $table->decimal('relief');
+                $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+                $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            }
+        );
     }
 
     /**
@@ -29,8 +34,8 @@ class CreateStrategiesTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
+
         Schema::dropIfExists('strategies');
     }
 }
