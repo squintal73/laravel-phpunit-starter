b65f7adbeffaa4969454e68e1db6a2e1677504fa
Joseph Udonsak <joseph.udonsak@live.co.uk>
2020-11-14 07:48:23 +0100

    Delete example test cases

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -3,17 +3,14 @@
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Application;
 
-trait CreatesApplication
-{
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
+trait CreatesApplication {
+
     public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
+    : Application {
+
+        $app = require __DIR__ . '/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
 
