a555e6691e0132681146d2aa0c2fcb0555ed7ec8
Joseph Udonsak <joseph.udonsak@live.co.uk>
2020-11-14 09:55:11 +0100

    Set up db and magic function for faker

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,31 @@
 
 namespace Tests;
 
+use Exception;
+use Faker\Factory;
+use Faker\Generator;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase {
 
-    use CreatesApplication;
+    use CreatesApplication, DatabaseMigrations;
+
+    private Generator $faker;
+
+    public function setUp()
+    : void {
+
+        parent::setUp();
+        $this->faker = Factory::create();
+        Artisan::call('db:seed');
+    }
+
+    public function __get($key) {
+
+        if ($key === 'faker')
+            return $this->faker;
+        throw new Exception('Unknown Key Requested');
+    }
 }
